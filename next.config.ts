edf3e94af06404763b4287cd160b336715521be5
Ranga Mudunkotuwa <rangabandara1729@gmail.com>
2026-08-16 21:52:29 +0530

    Initialize the project

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/auth',
+        destination: '/auth/signin',
+        permanent: true, // Set to false if it's a temporary redirect (307 vs 308)
+      },
+    ];
+  },
 };
 
 export default nextConfig;
