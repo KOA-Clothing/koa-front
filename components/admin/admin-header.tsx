c201ba91fd3b3f62760e1f2875fca96f4ad3124f
Ranga Mudunkotuwa <rangabandara1729@gmail.com>
2026-08-31 14:01:10 +0530

    Add route protectionb  based on the role in the admin dashboard

--- components/admin/admin-header.tsx
+++ components/admin/admin-header.tsx
@@ -3,7 +3,7 @@ import { SidebarTrigger } from "../ui/sidebar";
 
 export default function AdminHeader() {
   return (
-    <header className="h-15 flex items-center justify-between border-b px-4 bg-background">
+    <header className="h-13 flex items-center justify-between border-b px-4 bg-background">
       <SidebarTrigger />
       <Show when="signed-in">
         <div className="h-10 p-2 bg-neutral-100 hover:bg-neutral-200 rounded-md transition duration-300 flex items-center justify-center">
